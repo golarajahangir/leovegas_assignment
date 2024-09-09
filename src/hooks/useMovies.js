9313d046771fed8572f8b6b594375aad58bb25a8
Golara Jahangir <golara@homer.co>
2024-09-09 15:07:43 +0200

    Implement custom hook for movies and add pages folder

---
+++ src/hooks/useMovies.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMovies } from '../data/moviesSlice';
+import {
+  ENDPOINT_SEARCH,
+  ENDPOINT_DISCOVER,
+  ENDPOINT,
+  API_KEY,
+} from '../constants';
+import { createSearchParams, useNavigate } from 'react-router-dom';
+
+const useMovies = (searchParams, setSearchParams) => {
+  const state = useSelector((state) => state);
+  const { movies } = state;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const searchQuery = searchParams.get('search');
+
+  const [videoKey, setVideoKey] = useState();
+  const [isOpen, setOpen] = useState(false);
+
+  const closeModal = () => setOpen(false);
+
+  const getSearchResults = (query) => {
+    if (query !== '') {
+      dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=` + query));
+      setSearchParams(createSearchParams({ search: query }));
+    } else {
+      dispatch(fetchMovies(ENDPOINT_DISCOVER));
+      setSearchParams();
+    }
+  };
+
+  const searchMovies = (query) => {
+    navigate('/');
+    getSearchResults(query);
+  };
+
+  const getMovies = () => {
+    if (searchQuery) {
+      dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=` + searchQuery));
+    } else {
+      dispatch(fetchMovies(ENDPOINT_DISCOVER));
+    }
+  };
+
+  const viewTrailer = (movie) => {
+    getMovie(movie.id);
+    if (!videoKey) setOpen(true);
+    setOpen(true);
+  };
+
+  const getMovie = async (id) => {
+    const URL = `${ENDPOINT}/movie/${id}?api_key=${API_KEY}&append_to_response=videos`;
+
+    setVideoKey(null);
+    const videoData = await fetch(URL).then((response) => response.json());
+
+    if (videoData.videos && videoData.videos.results.length) {
+      const trailer = videoData.videos.results.find(
+        (vid) => vid.type === 'Trailer'
+      );
+      setVideoKey(trailer ? trailer.key : videoData.videos.results[0].key);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, [searchQuery]);
+
+  return {
+    movies,
+    searchMovies,
+    viewTrailer,
+    videoKey,
+    isOpen,
+    closeModal,
+  };
+};
+
+export default useMovies;
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,27 +1,20 @@
-import Movie from './Movie';
+import Movie from '../components/Movie';
 import '../styles/movies.scss';
 
 import { useSelector } from 'react-redux';
 import useInfiniteScroll from '../hooks/useInfiniteScroll';
 
-const Movies = ({ viewTrailer, closeCard }) => {
+const MoviesPage = ({ viewTrailer }) => {
   const { movies, fetchStatus } = useSelector((state) => state.movies);
   useInfiniteScroll();
   return (
     <div data-testid='movies' className='movie-grid'>
       {movies?.map((movie) => {
-        return (
-          <Movie
-            movie={movie}
-            key={movie.id}
-            viewTrailer={viewTrailer}
-            closeCard={closeCard}
-          />
-        );
+        return <Movie movie={movie} key={movie.id} viewTrailer={viewTrailer} />;
       })}
       {fetchStatus === 'loading' && <p>Loading more movies...</p>}
     </div>
   );
 };
 
-export default Movies;
+export default MoviesPage;
